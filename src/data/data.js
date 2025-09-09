93cf7a3b271ef9302fe188303fab66b46b97d7fa
stivcode21 <stivenelcodificador@gmail.com>
2025-09-08 23:00:56 -0500

    feat: actually data with all images

---
+++ src/data/data.js
@@ -0,0 +1,104 @@
+import posterMirame from "../assets/posters/poster-mirame.webp";
+import posterLejania from "../assets/posters/poster-lejania.webp";
+import posterJordan from "../assets/posters/poster-jordan.webp";
+import posterMonastery from "../assets/posters/poster-monastery.webp";
+import posterMojandoAsientos from "../assets/posters/poster-mojandoAsientos.webp";
+import posterSoltera from "../assets/posters/poster-soltera.webp";
+import posterPolosOpuestos from "../assets/posters/poster-polosOpuestos.webp";
+import posterLuna from "../assets/posters/poster-luna.webp";
+import posterUwaie from "../assets/posters/poster-uwaie.webp";
+import posterNormal from "../assets/posters/poster-normal.webp";
+
+import bannerMirame from "../assets/banners/banner-mirame.webp";
+import bannerLejania from "../assets/banners/banner-lejania.webp";
+import bannerJordan from "../assets/banners/banner-jordan.webp";
+import bannerMonastery from "../assets/banners/banner-monastery.webp";
+import bannerSoltera from "../assets/banners/banner-soltera.webp";
+import bannerMojandoAsientos from "../assets/banners/banner-mojandoAsientos.webp";
+import bannerPolosOpuestos from "../assets/banners/banner-polosOpuestos.webp";
+import bannerLuna from "../assets/banners/banner-luna.webp";
+import bannerUwaie from "../assets/banners/banner-uwaie.webp";
+import bannerNormal from "../assets/banners/banner-normal.webp";
+
+export const allSongs = [
+  {
+    id: 0,
+    title: "MÍRAME ",
+    artist: "Blessd",
+    img: posterMirame,
+    banner: bannerMirame,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/BLESSD%20%20OVY%20ON%20THE%20DRUMS%20-%20M%C3%8DRAME%20%20(VIDEO%20OFICIAL).mp3?alt=media&token=d06b4816-54e8-47f3-a95d-a7226de1491b",
+  },
+  {
+    id: 1,
+    title: "Lejania",
+    artist: "Blessd, Ryan Castro",
+    img: posterLejania,
+    banner: bannerLejania,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/Ryan%20Castro%20X%20Blessed%20%20%20LEJAN%C3%8DA%20%20(Video%20Oficial).mp3?alt=media&token=203e4bde-6f3b-44f9-9e83-b1e974ce5abc",
+  },
+  {
+    id: 2,
+    title: "JORDAN",
+    artist: "Ryan Castro",
+    img: posterJordan,
+    banner: bannerJordan,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/Ryan%20Castro%20-%20JORDAN%20%20(Video%20Oficial).mp3?alt=media&token=ce8cd8c6-8e9e-46d5-9708-e1d69b16c96f",
+  },
+  {
+    id: 3,
+    title: "MONASTERY",
+    artist: "Ryan Castro, Feid",
+    img: posterMonastery,
+    banner: bannerMonastery,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/Ryan%20Castro%2C%20Feid%20-%20Monastery%20(V%C3%ADdeo%20Oficial).mp3?alt=media&token=6a2498d6-dd87-4517-9595-5209c8c999b0",
+  },
+  {
+    id: 4,
+    title: "MOJANDO ASIENTOS",
+    artist: "Maluma",
+    img: posterMojandoAsientos,
+    banner: bannerMojandoAsientos,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/Maluma%20-%20Mojando%20Asientos%20(Official%20Video)%20ft.%20Feid.mp3?alt=media&token=884a52a9-18ea-467e-9ad4-6566434ccc75",
+  },
+  {
+    id: 5,
+    title: "POLOS OPUESTOS",
+    artist: "Blessd, Kris R",
+    img: posterPolosOpuestos,
+    banner: bannerPolosOpuestos,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/BLESSD%20%20KRIS%20R%20%20POLOS%20OPUESTOS%20REMIX%20%20(VIDEO%20OFICIAL).mp3?alt=media&token=af7acb98-7633-42fc-add4-7a746820f0ff",
+  },
+  {
+    id: 6,
+    title: "SOLTERA",
+    artist: "Blessd, Westcol",
+    img: posterSoltera,
+    banner: bannerSoltera,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/W%20Sound%2001%20Soltera%20-%20Blessd%2C%20Westcol%2C%20Ovy%20On%20The%20Drums%20(Official%20Video).mp3?alt=media&token=4013433e-0fe6-444d-b4f0-cf200416a825",
+  },
+  {
+    id: 7,
+    title: "LUNA",
+    artist: "Feid",
+    img: posterLuna,
+    banner: bannerLuna,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/Feid%2C%20ATL%20Jacob%20-%20LUNA%20(Official%20Video).mp3?alt=media&token=b0d5da05-5397-4739-87c5-b518607c4603",
+  },
+  {
+    id: 8,
+    title: "UWAIE",
+    artist: "Kapo",
+    poster: posterUwaie,
+    banner: bannerUwaie,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/UWAIE%20-%20Kapo%20(Video%20Oficial).mp3?alt=media&token=81eb4ea4-62e6-4ef7-a52a-71655c40d8a4",
+  },
+  {
+    id: 9,
+    title: "NORMAL",
+    artist: "Feid",
+    img: posterNormal,
+    banner: bannerNormal,
+    src: "https://firebasestorage.googleapis.com/v0/b/base-batos-shop.appspot.com/o/Feid%20-%20Normal%20(Official%20Video).mp3?alt=media&token=7709b0a4-7897-461d-8d52-16370472a2b6",
+  },
+];
