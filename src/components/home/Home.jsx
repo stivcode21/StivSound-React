8845f8faab3fdd25f24d48c4b6b3b962d24bc2d5
stivcode21 <stivenelcodificador@gmail.com>
2025-09-09 13:41:47 -0500

    feat: import musicPlayer in home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,12 @@
-import PlayerBar from "../playerBar/PlayerBar";
+import MusicPlayer from "../musicPlayer/MusicPlayer";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  return <PlayerBar />;
+  return (
+    <div styles={styles.container}>
+      <MusicPlayer />
+    </div>
+  );
 };
 
 export default Home;
