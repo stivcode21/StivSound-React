84580e6eba2d38ba33ecff43ad0cfc6821f5214a
stivcode21 <stivenelcodificador@gmail.com>
2025-09-10 21:48:46 -0500

    feat: new desing in TopBar

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -2,17 +2,17 @@ import styles from "./TopBar.module.css";
 
 const TopBar = ({ name }) => {
   return (
-    <div className={styles.playerBar}>
+    <header className={styles.playerBar}>
       <div className={styles.parallelLines}>
-        <div></div>
-        <div></div>
+        <div className={styles.bar1}></div>
+        <div className={styles.bar2}></div>
       </div>
       <h1 className={styles.title}>{name}</h1>
       <div className={styles.parallelLines}>
-        <div></div>
-        <div></div>
+        <div className={styles.bar1}></div>
+        <div className={styles.bar2}></div>
       </div>
-    </div>
+    </header>
   );
 };
 
